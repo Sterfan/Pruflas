ce73476b370ce9a736a4b713417ef623d03e9d9d
Unknown <wowonfire@hotmail.com>
2019-03-07 13:10:16 +0100

    Audio and Bullet Explosions Mostly
    Some script changes. Let me know if there are any merging conflicts. Love you. Bye. TTYL. @ me.

--- Elemental Es-qep/Assets/Scripts/DeleteBullet.cs
+++ Elemental Es-qep/Assets/Scripts/DeleteBullet.cs
@@ -6,14 +6,22 @@ public class DeleteBullet : MonoBehaviour
 {
 
     float deleteTime = 2f;
-    
+    public GameObject flishFlash;
+
+
     void Start()
     {
-        
+        Vector2 offset = transform.position;
+        offset.y += 20;
     }
 
     private void OnTriggerEnter2D()
     {
+        Vector2 offset = transform.position;
+        offset.x += 0.4f;
+        offset.y += 0.5f;
+
+        Instantiate(flishFlash, offset, transform.rotation);
         Destroy(gameObject);
     }
 
