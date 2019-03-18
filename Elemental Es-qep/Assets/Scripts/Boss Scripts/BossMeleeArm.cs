16eb1c604d8449a1b918816413c5134c7976a10a
MaherJ <Maherj.jaber23@gmail.com>
2019-03-18 14:47:34 +0100

    rough presentation of the boss's melee attack
    i will improve it , fix the bugs with it and smooth everything in the boss scene this week

--- Elemental Es-qep/Assets/Scripts/Boss Scripts/BossMeleeArm.cs
+++ Elemental Es-qep/Assets/Scripts/Boss Scripts/BossMeleeArm.cs
@@ -4,52 +4,48 @@ using UnityEngine;
 
 public class BossMeleeArm : MonoBehaviour
 {
-    //public GameObject detectionLine;
-    
-    public float moveSpeed = 10;
-    public GameObject pointA;
-    public GameObject pointB;
-    private bool reverseMove = true;
-    public Transform objectToUse;
-    public bool allowedToMove = false;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private GameObject pointA;
+    [SerializeField] private GameObject pointB;
+    [SerializeField] private bool reverseMove = false;
+    [SerializeField] private Transform objectToUse;
+    [SerializeField] private bool moveThisObject = false;
     private float startTime;
-    private float pathMovement;
-    private float distance;
-    private float path;
-
+    private float journeyLength;
+    private float distCovered;
+    private float fracJourney;
     void Start()
     {
-        startTime = Time.deltaTime;
-
-        pathMovement = Vector3.Distance(pointA.transform.position, pointB.transform.position);
+        startTime = Time.time;
+        if (moveThisObject)
+        {
+            objectToUse = transform;
+        }
+        journeyLength = Vector3.Distance(pointA.transform.position, pointB.transform.position);
     }
-
-    
     void Update()
+    {
+        distCovered = (Time.time - startTime) * moveSpeed;
+        fracJourney = distCovered / journeyLength;
+        if (reverseMove)
         {
-        if (allowedToMove == true)
+            objectToUse.position = Vector3.Lerp(pointB.transform.position, pointA.transform.position, fracJourney);
+        }
+        else
         {
-            distance = (Time.time - startTime) * moveSpeed;
-            path = distance / pathMovement;
+            objectToUse.position = Vector3.Lerp(pointA.transform.position, pointB.transform.position, fracJourney);
+        }
+        if ((Vector3.Distance(objectToUse.position, pointB.transform.position) == 0.0f || Vector3.Distance(objectToUse.position, pointA.transform.position) == 0.0f)) //Checks if the object has travelled to one of the points
+        {
             if (reverseMove)
             {
-                objectToUse.position = Vector3.Lerp(pointB.transform.position, pointA.transform.position, path);
-            }
-            else
-            {
-                objectToUse.position = Vector3.Lerp(pointA.transform.position, pointB.transform.position, path);
-            }
-            if ((Vector3.Distance(objectToUse.position, pointB.transform.position) == 0.0f))//|| Vector3.Distance(objectToUse.position, pointA.transform.position) == 0.0f)) //Checks if the object has travelled to one of the points
-            {
-
                 reverseMove = false;
-
-                startTime = Time.time;
             }
             else
             {
                 reverseMove = true;
             }
+            startTime = Time.time;
         }
-        }
+    }
 }
--- Elemental Es-qep/Assets/Scripts/Boss Scripts/PlayerDetection.cs
+++ Elemental Es-qep/Assets/Scripts/Boss Scripts/PlayerDetection.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class PlayerDetection : MonoBehaviour
-{
-    private BossMeleeArm allowed;
+//public class PlayerDetection : MonoBehaviour
+//{
+//    private BossMeleeArm allowed;
 
-    void Start()
-    {
+//    void Start()
+//    {
         
-    }
+//    }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            allowed.GetComponent<BossMeleeArm>().allowedToMove = true;
-        }
-    }
+//    void OnTriggerEnter2D(Collider2D other)
+//    {
+//        if (other.gameObject.tag == "Player")
+//        {
+//            allowed.GetComponent<BossMeleeArm>().allowedToMove = true;
+//        }
+//    }
     
     
-}
+//}
