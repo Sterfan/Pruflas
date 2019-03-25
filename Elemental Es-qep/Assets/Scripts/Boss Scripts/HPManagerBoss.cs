6d478c1ed39c0e5d35a554a88da41123e280d688
MaherJ <Maherj.jaber23@gmail.com>
2019-03-25 09:29:26 +0100

    added boss HP bar .. more work here and there

--- Elemental Es-qep/Assets/Scripts/Boss Scripts/HPManagerBoss.cs
+++ Elemental Es-qep/Assets/Scripts/Boss Scripts/HPManagerBoss.cs
@@ -2,43 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class HPManagerBoss : MonoBehaviour
 {
     [SerializeField]
-    private int maxHealth = 20;
-    public int currentHealth;
+    private float maxHealth = 20;
+    public float currentHealth;
     public Slider healthBar;
+    public GameObject deathAnimation;
+    public float waitForIT = 1;
+    public float cooldown = 1;
+    public static bool Win;
 
     void Start()
     {
         currentHealth = maxHealth;
+        Win = false;
     }
 
+    void Update()
+    {
 
-    
+        if (currentHealth <= 0)
+        {
+            cooldown -= Time.deltaTime;
+            Win = true;
+
+            Instantiate(deathAnimation, transform.position, transform.rotation);
+
+            if (cooldown <= 0)
+            {
+                Destroy(gameObject);
+                cooldown = waitForIT;
+                SceneManager.LoadScene("WinScreen");
+            }
+
+
+        }
+        
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "PlayerBullet")
         {
             currentHealth--;
-            
+
         }
 
     }
-    void Update()
-    {
 
-        //healthBar.value = currentHealth;
-
-        if (currentHealth <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
     public void TakingDamage(int damagetaken)
     {
         currentHealth -= damagetaken;
     }
 }
+
+
+
+
+
