b5ca1ed7211afc37ce8aa4ea469a4e5c99f36d46
MaherJ <Maherj.jaber23@gmail.com>
2019-03-27 01:00:23 +0100

    updated animations and other fixes

---
+++ Elemental Es-qep/Assets/Scripts/newScripts/meleeattackboss.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meleeattackboss : MonoBehaviour
+{
+    public Transform[] points;
+    public float speed = 5f;
+    Transform currentPoint;
+    int currentPointIndex;
+
+
+
+    void Start()
+    {
+        currentPointIndex = 0;
+        currentPoint = points[currentPointIndex];
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        if (Vector3.Distance (transform.position,currentPoint.position) < .1f)
+        {
+            if (currentPointIndex +1 < points.Length)
+            {
+                currentPointIndex++;
+            }
+            else
+            {
+                currentPointIndex = 0;
+            }
+            currentPoint = points[currentPointIndex];
+        }
+        Vector3 pointdirection = currentPoint.position - transform.position;
+        float angle = Mathf.Atan2(pointdirection.y, pointdirection.x) * Mathf.Rad2Deg - 180f;
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 180f);
+    }
+}
